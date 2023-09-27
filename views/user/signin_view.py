aa35b5aa7a31ed42d572169d69fe09f883a7a42e
with-developer <miki3079@naver.com>
2023-09-27 17:22:01 +0900

    로그인 기능 라우팅 경로 수정 및 html 스켈레톤 코드 추가

--- views/user/signin_view.py
+++ views/user/signin_view.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 
 signin_blueprint = Blueprint('signin', __name__, url_prefix='/')
 # signin root route: /
