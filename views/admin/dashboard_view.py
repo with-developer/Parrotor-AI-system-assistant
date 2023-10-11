89c9bc8b57ef65aeacf3044246e9ce2b67e4579b
with-developer <miki3079@naver.com>
2023-10-11 16:14:57 +0900

    디자인 변경 및 관리자용 페이지 접근 버튼 추가

--- views/admin/dashboard_view.py
+++ views/admin/dashboard_view.py
@@ -2,9 +2,9 @@ from flask import Blueprint, render_template, jsonify
 from ..API.account.account_verification_api import check_verification
 
 admin_dashboard_blueprint = Blueprint('admin_dashboard', __name__, url_prefix='/jiranistrator/dashboard')
-# admin dashboard root route: /
+# admin dashboard root route: /jiranistrator/dashboard
 
 @admin_dashboard_blueprint.route('/', methods=['GET', 'POST'])
 @check_verification(['admin'])
 def signin():
-    return render_template('admin/dashboard.html')
+    return render_template('admin/dashboard.html', page_title="관리자/대시보드")
