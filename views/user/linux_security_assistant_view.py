537638c001a7dca83014467a5b9acf524ad43a59
with-developer <miki3079@naver.com>
2023-11-21 10:27:27 +0900

    리눅스 보안 어시스턴트 view 추가

---
+++ views/user/linux_security_assistant_view.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template
+from ..API.account.account_verification_api import check_verification
+from flask import g
+
+linux_security_assistant_blueprint = Blueprint('linux_security_assistant', __name__, url_prefix='/linux_security_assistant')
+# linux_security_assistant route: /linux_security_assistant
+
+@linux_security_assistant_blueprint.route('/', methods=['GET'])
+@check_verification(['user', 'admin'])
+def linux_security_assistant():
+    is_admin = True if g.user_role == "admin" else False
+    return render_template('user/linux_security_assistant.html', page_title="사용자/리눅스 보안 어시스턴트", is_admin=is_admin)
